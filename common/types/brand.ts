a9709741d9f4bf6be72ae6508986597487ec3b3d
Nathan Wall <nathan@wall.coffee>
2025-11-14 06:06:48 +0000

    Define some simple propane files for testing

---
+++ common/types/brand.ts
@@ -0,0 +1,10 @@
+export const $brand = Symbol('brand');
+
+export type Brand<T, B> = T & { readonly [$brand]: B };
+
+export const brand = <B>(b: B) => <T>(t: T): Brand<T, B> => t as Brand<T, B>;
+
+export const unbrand = <B>(b: B) => <T>(t: Brand<T, B>): T => t as T;
+
+export const isBrand = <B>(t: unknown): t is Brand<unknown, B> =>
+  $brand in (t as object);
